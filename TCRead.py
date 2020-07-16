fa06cec82ef9bf11377f0bbcd3abb29d30b89d97
Matt Goode <mattgoode0@gmail.com>
2020-07-16 18:29:13 -0500

    Updated TC record to only record when step starts

--- TCRead.py
+++ TCRead.py
@@ -9,18 +9,20 @@ ser = serial.Serial('COM7', 9600)
 
 
 def get_temp(steps, wait, name):
-    time.sleep(1)  # waits for 1 second as the buzzer beeps for 1 second to singal start of test
+    time.sleep(1)  # waits for 1 second as the buzzer beeps for 1 second to signal start of test
     data_storage = [datetime.datetime.now()]
     data_storage.append('Steps: ' + str(steps))
     data_storage.append('Wait time: ' + str(wait))
-    runtime = float(steps) * float(wait)
-    end_time = datetime.datetime.now() + datetime.timedelta(0, runtime)
+    i = 1
 
-    while datetime.datetime.now() < end_time:
+    # record TC at each step of the sweep
+    while i <= int(steps):
         raw_temp = str(ser.readline())
-        temp = float(raw_temp[2:7])
+        temp = i + "," + float(raw_temp[2:7])
         data_storage.append(temp)
+        time.sleep(int(wait))
+        i += 1
 
     df = pd.DataFrame(data_storage)
-    file_name = 'SAMPLE_' + str(name) + '.csv'
+    file_name = 'SAMPLETC_' + str(name) + '.csv'
     df.to_csv(file_name, index=False)
